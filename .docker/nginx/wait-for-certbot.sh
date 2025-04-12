0e3f24aa1908ac325a1bce9e277f3e7d51817eab
Luis Meirelles <luis.gustavo@essentia.com.br>
2025-04-11 21:59:47 -0300

    refactor: fix docker (uses https://github.com/LuisMeirelles/proxy.git)

--- .docker/nginx/wait-for-certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while [ ! -f "/etc/letsencrypt/live/${SERVER_NAME}/fullchain.pem" ] && [ ! -f "/etc/ssl/certs/selfsigned.crt" ]; do
-    echo "Waiting for SSL certificates..."
-    sleep 5
-done
-
-echo "Certificates found, starting NGINX..."
-nginx -g "daemon off;"
